8cd678382179f37b72a87f569c05d2d39a226dc5
NizarAbiZaher <140507264+NizarAbiZaher@users.noreply.github.com>
2024-04-02 22:28:18 -0400

    removed '

--- edit/components/edited-shorts.tsx
+++ edit/components/edited-shorts.tsx
@@ -17,7 +17,7 @@ export const EditedShorts = () => {
     }
     return (
         <div className='text-center'>
-            <h1 className='pb-4 font-bold tracking-tight text-5xl lg:text-6xl'>Shorts I've Edited</h1>
+            <h1 className='pb-4 font-bold tracking-tight text-5xl lg:text-6xl'>Shorts Ive Edited</h1>
             <div className='flex items-center justify-center'>
                 <Separator className='mt-3 bg-slate-100/20 h-0.5 w-40' />
             </div>
